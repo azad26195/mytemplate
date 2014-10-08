2a45b4787faf57ee130233ac63007df6095bf10e
azad <azad26195@gmail.com>
2014-10-08 09:35:35 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,24 @@
+for _ in xrange(input()):
+	n=int(raw_input())
+	b=map(int,raw_input().split())
+	di=[]
+	vi=[]
+	c=0
+	for i in b:
+		if(i<0):
+			di.append(b.index(i))
+		else:
+			vi.append(b.index(i))
+	# print di,vi
+	di=sorted(di,reverse=True)
+	vi=sorted(vi,reverse=True)
+	#print di,vi
+	p=len(di)
+	q=len(vi)
+	while(p):
+		while(q)
+			if (x-y)<0:
+				c+=b[y]*abs(x-y)
+				q-=1
+				
+			else 
